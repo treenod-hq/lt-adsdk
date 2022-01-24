2c47cb01ede18e9c6464f019608fc1ca2069a4c4
Dylan <youngsoo@treenod.com>
2022-01-24 16:31:18 +0900

    데이터 파일 생성메뉴 수정

--- Editor/AppLovinSettingDataCreator.cs
+++ Editor/AppLovinSettingDataCreator.cs
@@ -7,18 +7,43 @@ namespace Treenod.Ad
     public class AppLovinSettingDataCreator
     {
         [MenuItem( "AdSdk/AppLovin/셋팅데이터 생성" )]
-        private static void CreateAppLovinSettingData ()
+        private static void CreateAppLovinSettings ()
         {
+            CreateAppLovinSettingData();
+            CreateAppLovinSettingFile();
+            
+            AssetDatabase.Refresh();
+            EditorUtility.DisplayDialog("알림", "파일을 생성했습니다.", "확인");
+        }
+
+        private static void CreateAppLovinSettingData()
+        {
+            string filePath = string.Format("{0}{1}.asset", AppLovinSettingData.DIRECTORY, AppLovinSettingData.FILENAME);
+            
             if ( !Directory.Exists( AppLovinSettingData.DIRECTORY ) )
             {
                 Directory.CreateDirectory( AppLovinSettingData.DIRECTORY );
             }
-
-            string path = AssetDatabase.GenerateUniqueAssetPath( string.Format( "{0}{1}.asset", AppLovinSettingData.DIRECTORY, AppLovinSettingData.FILENAME ) );
+            
+            if (AssetDatabase.LoadAssetAtPath<AppLovinSettingData>( filePath ) != null)
+            {
+                return;
+            }
+            
+            string path = AssetDatabase.GenerateUniqueAssetPath( filePath );
 
             AppLovinSettingData asset = ScriptableObject.CreateInstance<AppLovinSettingData>();
             AssetDatabase.CreateAsset( asset, path );
-            AssetDatabase.Refresh();
+        }
+
+        private static void CreateAppLovinSettingFile()
+        {
+            if ( !Directory.Exists( "Assets/MaxSdk" ) )
+            {
+                Directory.CreateDirectory( "Assets/MaxSdk" );
+            }
+
+            AppLovinSettings settings = AppLovinSettings.Instance;
         }
     }
 }
